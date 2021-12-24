e3425bc7a1162b169750056316a3e2dedcd88f07
Anshuman <anshuman8800@gmail.com>
2021-12-25 00:24:49 +0530

    Include Header and Footer component

---
+++ olvy_user_list/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Footer -->
+  <mdb-footer color="black" class="font-small pt-0">
+    <div class="footer-copyright text-center py-3">
+      <mdb-container fluid>
+        &copy; 2021 Olvy Inc. All rights reserved.
+      </mdb-container>
+    </div>
+  </mdb-footer>
+  <!-- Footer -->
+</template>
+
+<script>
+import { mdbFooter, mdbContainer } from "mdbvue";
+export default {
+  name: "Footer",
+  components: {
+    mdbFooter,
+    mdbContainer,
+  },
+};
+</script>
