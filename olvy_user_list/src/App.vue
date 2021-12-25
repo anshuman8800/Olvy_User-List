8e6f263aa4e146253920be6ccbc96a7e1e178ef9
Anshuman <anshuman8800@gmail.com>
2021-12-25 23:51:47 +0530

    Add sample Data and Body component.

--- olvy_user_list/src/App.vue
+++ olvy_user_list/src/App.vue
@@ -2,18 +2,13 @@
   <div id="app">
     <Header />
     <div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld />
-    <Body></Body>
+      <Body></Body>
     </div>
-    <div>
-      <Footer />
-    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Header from "./components/Header.vue";
 import Body from "./components/Body.vue";
 import Footer from "./components/Footer.vue";
@@ -21,7 +16,6 @@ import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Header,
     Body,
     Footer,
@@ -38,6 +32,5 @@ export default {
   color: #2c3e50;
   background-image: url("https://olvy.s3.amazonaws.com/hero-background.svg");
   background-size: cover;
-  /* margin-top: 60px; */
 }
 </style>
---
+++ olvy_user_list/src/components/Body.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="root">
+    <div>
+      <h1 id="heading" class="font-weight-bold">
+        Welcome to Olvy
+        <span style="color: #ff3a7c"> User</span>
+        <span style="color: voilet"> List </span>
+        <span>
+          <sup>
+            <b-spinner
+              style="color: #ff3a7c"
+              small
+              label="Small Spinner"
+              type="grow"
+            ></b-spinner>
+          </sup>
+        </span>
+      </h1>
+    </div>
+
+    <div id="dat_list">
+      <DataList />
+    </div>
+  </div>
+</template>
+
+<script>
+import DataList from "./DataList.vue";
+
+export default {
+  name: "Body",
+  components: {
+    DataList,
+  },
+};
+</script>
+
+<style scoped>
+#heading {
+  color: navy;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+#data_list {
+  padding-left: 6rem;
+  padding-right: 6rem;
+}
+</style>
